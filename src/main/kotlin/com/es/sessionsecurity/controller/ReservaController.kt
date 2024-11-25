b601c889424396819eae9c2592aa40a9ed090e5d
Lainezz <diegolinaresmojacar@gmail.com>
2024-11-25 17:37:10 +0100

    adicion de enunciado

--- src/main/kotlin/com/es/sessionsecurity/controller/ReservaController.kt
+++ src/main/kotlin/com/es/sessionsecurity/controller/ReservaController.kt
@@ -2,6 +2,9 @@ package com.es.sessionsecurity.controller
 
 import com.es.sessionsecurity.model.Reserva
 import com.es.sessionsecurity.service.ReservaService
+import com.es.sessionsecurity.service.SessionService
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,24 +21,30 @@ class ReservaController {
 
     @Autowired
     private lateinit var reservaService: ReservaService
+    @Autowired
+    private lateinit var sessionService: SessionService
 
     /*
     OBTENER TODAS LAS RESERVAS POR EL NOMBRE DE USUARIO DE UN CLIENTE
      */
     @GetMapping("/{nombre}")
     fun getByNombreUsuario(
-        @PathVariable nombreUsuario: String
+        @PathVariable nombre: String,
+        request: HttpServletRequest
     ) : ResponseEntity<List<Reserva>?> {
 
         /*
         COMPROBAR QUE LA PETICIÓN ESTÁ CORRECTAMENTE AUTORIZADA PARA REALIZAR ESTA OPERACIÓN
          */
-        // CÓDIGO AQUÍ
+        // 1º Extraemos el token de la Cookie
+        val cookie: Cookie? = request.cookies.find { c: Cookie? ->  c?.name == "tokenSession"}
+        val token = cookie?.value
 
-        /*
-        LLAMAR AL SERVICE PARA REALIZAR LA L.N. Y LA LLAMADA A LA BASE DE DATOS
-         */
-        // CÓDIGO AQUÍ
+        // 2º Comprobar la validez del token
+        if (sessionService.checkToken(token)) {
+            // REALIZA LA CONSULTA A LA BASE DE DATOS
+
+        }
 
         // RESPUESTA
         return ResponseEntity<List<Reserva>?>(null, HttpStatus.OK); // cambiar null por las reservas
--- src/main/kotlin/com/es/sessionsecurity/service/SessionService.kt
+++ src/main/kotlin/com/es/sessionsecurity/service/SessionService.kt
@@ -1,8 +1,11 @@
 package com.es.sessionsecurity.service
 
+import com.es.sessionsecurity.error.exception.BadRequestException
+import com.es.sessionsecurity.model.Session
 import com.es.sessionsecurity.repository.SessionRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class SessionService {
@@ -10,8 +13,19 @@ class SessionService {
     @Autowired
     private lateinit var sessionRepository: SessionRepository
 
-    fun checkToken() : Boolean {
-        return true
+    fun checkToken(token: String?) : Boolean {
+
+        // 0º comprobar nullable
+        if (token == null) throw BadRequestException("Token invalido")
+
+        // 1º Vamos a obtener la Session asociada al token
+        val s: Session = sessionRepository
+            .findByToken(token)
+            .orElseThrow{RuntimeException("Token inválido")}
+
+        // Por último, comprobamos que la fecha sea válida
+        return s.fechaExp.isAfter(LocalDateTime.now())
+
     }
 
 }
